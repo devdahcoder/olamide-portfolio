490586e1d38456ce00e69cfb8a7978e01a8a7a65
devdahcoder <adigunolamide200@gmail.com>
2023-10-19 20:08:27 +0100

    feat: added work section

--- src/component/sections/work/work.tsx
+++ src/component/sections/work/work.tsx
@@ -1,44 +1,75 @@
 import { Component, For } from "solid-js";
 import { workContent } from "../../../../contents";
+import UpArrowIcon from "../../../../icon/up-arrow-icon";
+import IconButton from "../../icon-button";
 import Image from "../../image";
 import "./work.scss";
-import IconButton from "../../icon-button";
 
 const Work: Component<{}> = () => {
 	return (
 		<div class="work--container">
 			<div class="work--sub--container">
+				<div class="work--header--container">
+					<div>
+						<p>Selected Work</p>
+					</div>
+				</div>
 				<div class="work--list">
 					<For each={workContent}>
 						{(props) => (
-                            <div class="work--item--container">
-                                
-                                <div class="work--item--details--container">
-                                    <div class="work--item--details">
-                                        <div class="work--item--company--name"><p>{ props?.companyName }</p></div>
-                                        <div class="work--item--description"><p>{ props.description }</p></div>
-                                        <div class="work--item-date"><p>{ props.date }</p></div>
-                                    </div>
-                                    <div class="work--item--icon--container">
-                                        {/* <IconButton /> */}hello world
-                                    </div>
-                                </div>
+							<div
+								class={`work--item--container ${props.className}`}
+							>
+								<div class="work--item--sub--container">
+									<div class="work--item--details--container">
+										<div class="work--item--details">
+											<div class="work--item--company--name">
+												<p>{props?.companyName}</p>
+											</div>
+											<div class="work--item--description">
+												<p>{props.description}</p>
+											</div>
+											<div class="work--item-date">
+												<p>{props.date}</p>
+											</div>
+										</div>
 
-                                <div class="work--item--image--container">
-                                    <div class="work--item--title">
-                                        <p>/{ props.title }</p>
-                                    </div>
-									<Image
-										imageSrc={props?.image?.imageSrc}
-										imageAlt={props?.image?.imageAlt}
-										imageClass={props?.image?.imageClass}
-										imageContainerClass={
-											props?.image?.imageContainerClass
-										}
-										imageSubContainerClass={
-											props?.image?.imageSubContainerClass
-										}
-									/>
+										<div class="work--item--icon--text--container">
+											<IconButton
+												icon={
+													<UpArrowIcon
+														width="17"
+														height="17"
+														class="work--item--icon"
+													/>
+												}
+												iconButtonClass="work--item--icon--button"
+												iconButtonContainerClass="work--item--icon--container"
+											/>
+											<p>About Project</p>
+										</div>
+									</div>
+
+									<div class="work--item--image--container">
+										<div class="work--item--title">
+											<p>/{props.title}</p>
+										</div>
+										<Image
+											imageSrc={props?.image?.imageSrc}
+											imageAlt={props?.image?.imageAlt}
+											imageClass={
+												props?.image?.imageClass
+											}
+											imageContainerClass={
+												props?.image
+													?.imageContainerClass
+											}
+											imageSubContainerClass={
+												props?.image
+													?.imageSubContainerClass
+											}
+										/>
+									</div>
 								</div>
 							</div>
 						)}
--- types/index.ts
+++ types/index.ts
@@ -34,5 +34,6 @@ export type WorkType = {
     date: string;
     link: string;
     companyName: string;
+    className: string;
 
 }
