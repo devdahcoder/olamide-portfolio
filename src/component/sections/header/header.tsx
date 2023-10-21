850a57df6ea4c2fd8f1af9c696ce84ea840bdb9a
devdahcoder <adigunolamide200@gmail.com>
2023-10-21 17:21:52 +0100

    feat: added header logo

--- src/component/sections/header/header.tsx
+++ src/component/sections/header/header.tsx
@@ -4,15 +4,15 @@ import "./header.scss";
 const Header: Component<{}> = (props) => {
 	return (
 		<div class="header--container">
-            <div class="header--sub--container">
-                <div class="header--logo--container">
-                    <p>Ola</p>
-                </div>
+			<div class="header--sub--container">
+				<div class="header--logo--container">
+					<div class="header--logo">
+						<p>Ola.</p>
+					</div>
+				</div>
 
-                <div class="header--utils--container">
-
-                </div>
-            </div>
+				<div class="header--utils--container"></div>
+			</div>
 		</div>
 	);
 };
