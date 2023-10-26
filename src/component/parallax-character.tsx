257923df9db2518a10e43b77347c44b5cb1acc33
devdahcoder <adigunolamide200@gmail.com>
2023-10-26 12:13:49 +0100

    feat added animation to hero role text

--- src/component/parallax-character.tsx
+++ src/component/parallax-character.tsx
@@ -1,9 +1,21 @@
 import { Component } from "solid-js";
 
-const ParallaxCharacter: Component<{}> = () => {
+const ParallaxCharacter: Component<{
+	index: number;
+	children: string;
+	class: string;
+	parallaxCharacterElement: HTMLDivElement[];
+}> = (props) => {
 	return (
-		<div>
-			<div></div>
+		<div
+			ref={(element) => props.parallaxCharacterElement.push(element)}
+			class={props.class}
+		>
+			{props.children === " " ? (
+				<div style={{ margin: "0rem 1rem" }}></div>
+			) : (
+				props.children
+			)}
 		</div>
 	);
 };
--- src/component/sections/hero/hero.tsx
+++ src/component/sections/hero/hero.tsx
@@ -1,18 +1,64 @@
-import { Component } from "solid-js";
+import gsap from "gsap";
+import { Component, For, createSignal, onMount } from "solid-js";
 import UpArrowIcon from "../../../../icon/up-arrow-icon";
-import IconButton from "../../icon-button";
 import Link from "../../link";
+import ParallaxCharacter from "../../parallax-character";
 import "./hero.scss";
 
+const animateHeroMainCharacters = (index: number) => {
+	gsap.fromTo(
+		[".hero--main--text"],
+		{ yPercent: 200 },
+		{ yPercent: 0, delay: 0.2 + index * 0.3 }
+	);
+};
+
+const animateParallaxCharacter = (target: HTMLDivElement, index: number) => {
+
+	gsap.fromTo(
+		target,
+		{ yPercent: 90 },
+		{
+			yPercent: 0,
+			duration: 2.5,
+			ease: "expo.out",
+			// ease: "bounce.out",
+			// ease: "back.out(2)",
+			delay: 0.1 + index * 0.1,
+		}
+	);
+
+}
+
 const Hero: Component<{}> = () => {
+	const [role] = createSignal<string>("Full-Stack Developer");
+	const parallaxCharacterElement: HTMLDivElement[] = [];
+
+	onMount(() => {
+		// animateHeroMainCharacters(1 + 1);
+		parallaxCharacterElement.forEach((element, index) => animateParallaxCharacter(element, index))
+	});
+
 	return (
 		<div class="hero--container">
 			<div class="hero--sub--container">
 				<div class="hero--text--container">
 					<div class="hero--main--text--container">
 						<div class="hero--main--text">
-							<p>Full-Stack Developer</p>
+							{/* <p>Full-Stack Developer</p> */}
 						</div>
+						<For each={role().trim().split("")}>
+							{(character, index) => (
+								<ParallaxCharacter
+									index={index()}
+									class="hero--main--text"
+									children={character}
+									parallaxCharacterElement={
+										parallaxCharacterElement
+									}
+								/>
+							)}
+						</For>
 					</div>
 
 					<div class="hero--sub--text--container">
@@ -31,9 +77,17 @@ const Hero: Component<{}> = () => {
 						</div>
 
 						<div class="hero--sub--text">
-							<p>Spanning both backend and frontend development, my expertise enables me to deliver holistic solutions that excel in functionality and user experience. When I'm not immersed in code, debugging, or reading tech articles, I'm sharing my latest tech interests and learning experiences through writing which you can check out the link to my blog posts down below. I am excited about the
-								limitless possibilities that lie ahead in the
-								ever-evolving tech landscape.
+							<p>
+								Spanning both backend and frontend development,
+								my expertise enables me to deliver holistic
+								solutions that excel in functionality and user
+								experience. When I'm not immersed in code,
+								debugging, or reading tech articles, I'm sharing
+								my latest tech interests and learning
+								experiences through writing which you can check
+								out the link to my blog posts down below. I am
+								excited about the limitless possibilities that
+								lie ahead in the ever-evolving tech landscape.
 							</p>
 						</div>
 					</div>
@@ -55,7 +109,10 @@ const Hero: Component<{}> = () => {
 				/>
 
 				<div class="hero--scroll--more--container">
-					<div role="button" class="hero--scroll--more--sub--container">
+					<div
+						role="button"
+						class="hero--scroll--more--sub--container"
+					>
 						<p>Scroll Down</p>
 
 						<UpArrowIcon
