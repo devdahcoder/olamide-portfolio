e8175088ffe2d9ef2dde9b4715366129d8b1554c
devdahcoder <adigunolamide200@gmail.com>
2023-12-05 00:40:40 +0100

    refactor: refactored navigatin section

--- src/component/sections/navigation/navigation.tsx
+++ src/component/sections/navigation/navigation.tsx
@@ -4,191 +4,11 @@ import { headerLinksContent } from "../../../../contents";
 import ParallaxCharacter from "../../parallax-character";
 import "./navigation.scss";
 
-let navigationContentContainerRef: HTMLDivElement | undefined;
 let navigationLinkRef: HTMLDivElement[][] = [];
 let navigationLinkContainerRef: HTMLDivElement[] = [];
 
-const animateLinkFocus = (
-	entry: Element,
-	link: HTMLAnchorElement,
-	isFocused: boolean
-) => {
-	const borderElement = entry.querySelector(
-		".navigation--link--container--border"
-	) as HTMLDivElement;
-	const linkSvgElement = link.querySelectorAll(".icon") as any;
-
-	if (isFocused) {
-		gsap.to(entry, { scale: 1.5 });
-		gsap.to(linkSvgElement, { scale: 0.8 });
-		gsap.to(borderElement, { xPercent: 0 });
-		gsap.to(link, { color: "white" });
-	} else {
-		gsap.to(entry, { scale: 0.8 });
-		gsap.to(linkSvgElement, { scale: 0 });
-		gsap.to(borderElement, { xPercent: -200 });
-		gsap.to(link, { color: "grey" });
-	}
-};
-
-const animateNavigationLink = (show: boolean) => {
-	const target = ".navigation--link--container";
-	const duration = 1;
-	const stagger = show ? 0.2 : -0.2;
-	const delay = show ? 0.5 : 0.1;
-
-	gsap.fromTo(
-		target,
-		{
-			yPercent: show ? 500 : 0,
-			opacity: show ? 0 : 1,
-			// scale: show ? 0.5 : 1
-		},
-		{
-			yPercent: show ? 0 : 500,
-			opacity: show ? 1 : 0,
-			scale: 1,
-			stagger,
-			duration,
-			delay,
-		}
-	);
-};
-
-const animateLinkContainer = (show: boolean) => {
-	const target = ".navigation--content--sub--container";
-	const duration = show ? 1.7 : 1;
-	const ease = "power4.out";
-
-	if (show) {
-		gsap.fromTo(
-			target,
-			{
-				opacity: 0,
-				display: "none",
-			},
-			{
-				opacity: 1,
-				display: "flex",
-				duration,
-				ease,
-			}
-		);
-	} else {
-		gsap.to(target, {
-			opacity: 0,
-			display: "none",
-			delay: 1,
-		});
-	}
-};
-
-const animateNavigationGrid = (show: boolean) => {
-	const target = ".navigation--grid";
-	const duration = 1.2;
-	const ease = "power4.out";
-
-	if (show === true) {
-		gsap.fromTo(
-			target,
-			{
-				yPercent: -100,
-			},
-			{
-				yPercent: 0,
-				stagger: 0.3,
-				duration,
-				ease,
-			}
-		);
-	} else {
-		gsap.to(target, {
-			yPercent: -100,
-			stagger: -0.2,
-			duration,
-			ease,
-			delay: 0.1,
-			onComplete: () => {
-				// You can perform additional actions here if needed
-			},
-		});
-	}
-};
-
-const animateNavigationContentContainer = (show: boolean) => {
-	const target = ".navigation--sub--container";
-	const duration = show ? 1.7 : 1;
-	const ease = "power4.out";
-
-	if (show) {
-		gsap.fromTo(
-			target,
-			{
-				opacity: 0,
-				display: "none",
-			},
-			{
-				opacity: 1,
-				display: "flex",
-				duration,
-				ease,
-			}
-		);
-	} else {
-		gsap.to(target, {
-			opacity: 0,
-			display: "none",
-			delay: 1,
-		});
-	}
-};
-
-const animateNavigationSubContainer = (show: boolean) => {
-	const target = ".navigation--sub--container";
-	const duration = show ? 1.7 : 1;
-	const ease = "power4.out";
-
-	if (show) {
-		gsap.fromTo(
-			target,
-			{
-				opacity: 0,
-				// visibility: "hidden",
-				// position: "relative",
-				// zIndex: "-100",
-				// pointerEvents: "none",
-				display: "none",
-			},
-			{
-				opacity: 1,
-				// top: 0,
-				// left: 0,
-				// visibility: "visible",
-				// position: "fixed",
-				// zIndex: "90",
-				// pointerEvents: "auto",
-				display: "flex",
-				duration,
-				ease,
-			}
-		);
-	} else {
-		gsap.to(target, {
-			opacity: 0,
-			// visibility: "hidden",
-			// position: "relative",
-			// zIndex: "-100",
-			// pointerEvents: "none",
-			display: "none",
-			// duration,
-			// ease,
-			delay: 1.2,
-		});
-	}
-};
-
-const animateNavigationContainer = (show: boolean) => {
-	const target = ".navigation--container";
+const animateNavigationMainContainer = (show: boolean) => {
+	const target = ".navigation--main--container";
 	const duration = 1.2;
 
 	if (show === true) {
@@ -227,6 +47,147 @@ const animateNavigationContainer = (show: boolean) => {
 	}
 };
 
+const animateNavigationSubContainer = (show: boolean) => {
+	const target = ".navigation--sub--container";
+	const duration = show ? 1.7 : 1;
+	const ease = "power4.out";
+
+	if (show) {
+		gsap.fromTo(
+			target,
+			{
+				opacity: 0,
+				pointerEvents: "none",
+				display: "none",
+			},
+			{
+				opacity: 1,
+				pointerEvents: "auto",
+				display: "flex",
+				duration,
+				ease,
+			}
+		);
+	} else {
+		gsap.to(target, {
+			opacity: 0,
+			pointerEvents: "none",
+			display: "none",
+			delay: 1.2,
+		});
+	}
+};
+
+const animateNavigationGrid = (show: boolean) => {
+	const target = ".navigation--grid";
+	const duration = 1.2;
+	const ease = "power4.out";
+	const yPercent = -100;
+
+	if (show === true) {
+		gsap.fromTo(
+			target,
+			{
+				yPercent,
+			},
+			{
+				yPercent: 0,
+				stagger: 0.3,
+				duration,
+				ease,
+			}
+		);
+	} else {
+		gsap.to(target, {
+			yPercent,
+			stagger: -0.2,
+			duration,
+			ease,
+			delay: 0.1,
+		});
+	}
+};
+
+const animateNavigationContentMainContainer = (show: boolean) => {
+	const target = ".navigation--content--main--container";
+	const duration = show ? 1.7 : 1;
+	const ease = "power4.out";
+
+	if (show) {
+		gsap.fromTo(
+			target,
+			{
+				opacity: 0,
+				display: "none",
+			},
+			{
+				opacity: 1,
+				display: "flex",
+				duration,
+				ease,
+			}
+		);
+	} else {
+		gsap.to(target, {
+			opacity: 0,
+			display: "none",
+			delay: 1,
+		});
+	}
+};
+
+const animateContentSubContainer = (show: boolean) => {
+	const target = ".navigation--content--sub--container";
+	const duration = show ? 1.7 : 1;
+	const ease = "power4.out";
+
+	if (show) {
+		gsap.fromTo(
+			target,
+			{
+				opacity: 0,
+				display: "none",
+			},
+			{
+				opacity: 1,
+				display: "flex",
+				duration,
+				ease,
+			}
+		);
+	} else {
+		gsap.to(target, {
+			opacity: 0,
+			display: "none",
+			delay: 1,
+		});
+	}
+};
+
+const animateNavigationLinkContainer = (show: boolean) => {
+	const target = ".navigation--link--container";
+	const duration = 1;
+	const stagger = show ? 0.2 : -0.2;
+	const delay = show ? 0.5 : 0.1;
+
+	gsap.fromTo(
+		target,
+		{
+			yPercent: show ? 500 : 0,
+			opacity: show ? 0 : 1,
+			// scale: show ? 0.5 : 1
+		},
+		{
+			yPercent: show ? 0 : 500,
+			opacity: show ? 1 : 0,
+			scale: 1,
+			stagger,
+			duration,
+			delay,
+		}
+	);
+};
+
 const animateBodyPosition = (show: boolean) => {
 	const target = "body";
 	const duration = 1;
@@ -236,6 +197,29 @@ const animateBodyPosition = (show: boolean) => {
 		: { position: "relative" };
 
 	gsap.fromTo(target, positionProps, { duration, ease });
+};
+
+const animateLinkFocus = (
+	entry: Element,
+	link: HTMLAnchorElement,
+	isFocused: boolean
+) => {
+	const borderElement = entry.querySelector(
+		".navigation--link--container--border"
+	) as HTMLDivElement;
+	const linkSvgElement = link.querySelectorAll(".icon") as any;
+
+	if (isFocused) {
+		gsap.to(entry, { scale: 1.5 });
+		gsap.to(linkSvgElement, { scale: 0.8 });
+		gsap.to(borderElement, { xPercent: 0 });
+		gsap.to(link, { color: "white" });
+	} else {
+		gsap.to(entry, { scale: 0.8 });
+		gsap.to(linkSvgElement, { scale: 0 });
+		gsap.to(borderElement, { xPercent: -200 });
+		gsap.to(link, { color: "grey" });
+	}
 };
 
 const resetAnchor = (element: HTMLDivElement) => {
@@ -253,13 +237,13 @@ const Navigation: Component<{ isNavigationOpen: Accessor<boolean> }> = (
 ) => {
 	createEffect(() => {
 		props.isNavigationOpen();
-		animateNavigationContainer(props.isNavigationOpen());
+		animateNavigationMainContainer(props.isNavigationOpen());
 		animateBodyPosition(props.isNavigationOpen());
-		animateNavigationSubContainer(props.isNavigationOpen());
-		animateNavigationContentContainer(props.isNavigationOpen());
 		animateNavigationGrid(props.isNavigationOpen());
-		animateLinkContainer(props.isNavigationOpen());
-		animateNavigationLink(props.isNavigationOpen());
+		animateNavigationSubContainer(props.isNavigationOpen());
+		animateNavigationContentMainContainer(props.isNavigationOpen());
+		animateContentSubContainer(props.isNavigationOpen());
+		animateNavigationLinkContainer(props.isNavigationOpen());
 
 		let observe = new IntersectionObserver(
 			(entries) => {
@@ -290,16 +274,13 @@ const Navigation: Component<{ isNavigationOpen: Accessor<boolean> }> = (
 	});
 
 	return (
-		<div class="navigation--container">
+		<div class="navigation--main--container">
 			<div class="navigation--sub--container">
 				<For each={Array.of(1, 2, 3, 4, 5, 6)}>
 					{() => <div class="navigation--grid"></div>}
 				</For>
 
-				<div
-					ref={navigationContentContainerRef}
-					class="navigation--content--container"
-				>
+				<div class="navigation--content--main--container">
 					<div class="navigation--content--sub--container">
 						<For each={headerLinksContent}>
 							{(props, index) => (
