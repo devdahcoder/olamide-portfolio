e5e3a7a30a97d7ba33c281f933f4ff131ceef667
Adigun Olamide <adigunolamide200@gmail.com>
2025-04-28 22:57:58 +0100

    refactor: clean up App.tsx by removing commented-out sections and improving component structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import { createSignal, onCleanup, onMount } from "solid-js";
 import Loader from "./component/loader.tsx";
+// import AboutMe from "./component/sections/about-me/about-me";
+// import About from "./component/sections/about/about";
+// import Experience from "./component/sections/experience/experience.tsx";
+// import Footer from "./component/sections/footer/footer";
+import Header from "./component/sections/header/header";
+import Hero from "./component/sections/hero/hero";
+// import Intro from "./component/sections/intro/intro";
 import Navigation from "./component/sections/navigation/navigation";
+// import Project from "./component/sections/project/project";
+// import Service from "./component/sections/service/service";
 import Lenis from "@studio-freight/lenis";
+// import Mantra from "./component/sections/mantra/mantra.tsx";
 
 function App() {
 	const [isNavigationOpen, setIsNavigationOpen] =
@@ -11,7 +21,6 @@ function App() {
 	// Initialize smooth scroll using Lenis
 	onMount(() => {
 		const lenis = new Lenis({
-			// smoothTouch: true,
 			duration: 1.2,
 			easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
 			smoothWheel: true,
@@ -41,7 +50,24 @@ function App() {
 				isNavigationOpen={isNavigationOpen}
 				setIsNavigationOpen={setIsNavigationOpen}
 			/>
-			
+			<Header
+				isLoadedComplete={isLoadedComplete}
+				isNavigationOpen={isNavigationOpen}
+				setIsNavigationOpen={setIsNavigationOpen}
+			/>
+
+			<Hero
+				isLoadedComplete={isLoadedComplete}
+				isNavigationOpen={isNavigationOpen()}
+			/>
+			{/* <AboutMe isLoadedComplete={isLoadedComplete} />
+			<Intro isLoadedComplete={isLoadedComplete} />
+			<About />
+			<Project isLoadedComplete={isLoadedComplete} />
+			<Mantra />
+			<Experience />
+			<Service isLoadedComplete={isLoadedComplete} />
+			<Footer /> */}
 		</div>
 	);
 }
